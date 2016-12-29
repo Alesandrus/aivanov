0c9ab65c4b964b6abb7f148e43ad9bae779a3c60
Alesandrus <raulon7@mail.ru>
2016-12-29 23:41:47 +0300

    add Test

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+ * Class Класс для вычисления арифметических операций + - * / ^.
+ * @author Alexander Ivanov
+ * @since 29.12.2016
+ * @version 1.0
+ */
+
 public class Calculate {
+	
+	/**
+	 * Вывод строки "Hello World" в консоль
+	 * @param args - args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
