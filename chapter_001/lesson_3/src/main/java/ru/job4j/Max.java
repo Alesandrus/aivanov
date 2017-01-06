c9bb41b6c28976f9f51d086e81f96d81ea60608f
Alesandrus <raulon7@mail.ru>
2017-01-06 16:32:28 +0300

    Add Max class for return maximum from two numbers

---
+++ chapter_001/lesson_3/src/main/java/ru/job4j/Max.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+/**
+ * Class for return maximum.
+ * @author Alexander Ivanov
+ * @since 05.01.2016
+ * @version 1.0
+ */
+
+public class Max {
+	/**
+	 * Calculating maximum from two numbers.
+	 * @param first - first number.
+	 * @param second - second number.
+	 * @return maximum from two numbers.
+	 */
+	public int max(int first, int second) {
+		return first >= second ? first : second;
+	}
+}
---
+++ chapter_001/lesson_3/src/test/java/ru/job4j/MaxTest.java
@@ -0,0 +1,40 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test of Max class.
+ *
+ * @author Alexander Ivanov
+ * @since 06.01.2016
+ * @version 1.0
+ */
+public class MaxTest {
+	/**
+	 * Test max. First > second.
+	 */
+	@Test
+	public void whenFirstMoreThanSecond() {
+		final int first = 10;
+		final int second = 5;
+		Max maximum = new Max();
+		final int checkMax = 10;
+		final int result = maximum.max(first, second);
+		assertThat(result, is(checkMax));
+	}
+
+	/**
+	 * Test max. First > second.
+	 */
+	@Test
+	public void whenFirstLessThanSecond() {
+		final int first = 7;
+		final int second = 12;
+		Max maximum = new Max();
+		final int checkMax = 12;
+		final int result = maximum.max(first, second);
+		assertThat(result, is(checkMax));
+	}
+}
