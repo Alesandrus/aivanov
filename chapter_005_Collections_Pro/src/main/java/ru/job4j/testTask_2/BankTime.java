0e6410fd44e2e0801d113cb44a06d3bacb219606
Alesandrus <raulon7@mail.ru>
2017-04-27 22:40:17 +0300

    Add javaDoc to testTask_2

--- chapter_005_Collections_Pro/src/main/java/ru/job4j/testTask_2/BankTime.java
+++ chapter_005_Collections_Pro/src/main/java/ru/job4j/testTask_2/BankTime.java
@@ -3,7 +3,6 @@ package ru.job4j.testTask_2;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -53,50 +52,12 @@ public class BankTime {
     }
 
     /**
-     * Get the minimum amount of time, when there are maximum number of customers.
-     * @return minimum time.
-     */
-    public long getMinTimeBeing() {
-        long minTimePeiod = closing - opening;
-        for (int i = 0; i < clientList.size(); i++) {
-            long outFirst = clientList.get(i).getOutcome();
-            for (int j = i + 1; j < clientList.size(); j++) {
-                long inSecond = clientList.get(j).getIncome();
-                long outSecond = clientList.get(j).getOutcome();
-                if (inSecond <= outFirst) {
-                    long out;
-                    if (outSecond > outFirst) {
-                        out = outFirst;
-                    } else {
-                        out = outSecond;
-                    }
-                    long period = out - inSecond;
-                    minTimePeiod = minTimePeiod > period ? period : minTimePeiod;
-                }
-            }
-        }
-        return minTimePeiod;
-    }
-
-    /**
-     * Get time for change closing time. It's need for iterate all periods.
-     * @param minTimePeriod for iterating.
-     * @return time for change closing time.
-     */
-    private long getRoundedClosingTimeForIterate(long minTimePeriod) {
-        long diferenceCloseAndOpen = closing - opening;
-        long factor = (long) Math.ceil(diferenceCloseAndOpen / minTimePeriod);
-        return opening + minTimePeriod * factor;
-    }
-
-    /**
      * Adding to list all periods with maximum number of clients.
      */
     private void addToListOfMaxPeriods() {
-        long minTimePeriod = getMinTimeBeing();
-        long roundedClosing = getRoundedClosingTimeForIterate(minTimePeriod);
+        long minTimePeriod = 100;
         int maxClients = 0;
-        for (long i = opening; i <= roundedClosing - minTimePeriod; i += minTimePeriod) {
+        for (long i = opening; i <= closing; i += minTimePeriod) {
             int count = 0;
             long endOfPeriod = i + minTimePeriod;
             for (int j = 0; j < clientList.size(); j++) {
@@ -157,55 +118,5 @@ public class BankTime {
             System.out.println(String.format("С %tH:%tM:%tS по %tH:%tM:%tS - %d клиентов",
                     in, in, in, out, out, out, listOfPeriods.get(i).getMaximumOfClients()));
         }
-    }
-
-    public static void main(String[] args) {
-        Calendar incomeTimeOfFirstClient = new GregorianCalendar(2017, 5, 26, 9,0);
-        Calendar outcomeTimeOfFirstClient = new GregorianCalendar(2017, 5, 26, 9,50);
-        Client firstClient = new Client(incomeTimeOfFirstClient.getTimeInMillis());
-        firstClient.setOutcome(outcomeTimeOfFirstClient.getTimeInMillis());
-
-        Calendar incomeTimeOfSecondClient = new GregorianCalendar(2017, 5, 26, 9,30);
-        Calendar outcomeTimeOfSecondClient = new GregorianCalendar(2017, 5, 26, 15,50);
-        Client secondClient = new Client(incomeTimeOfSecondClient.getTimeInMillis());
-        secondClient.setOutcome(outcomeTimeOfSecondClient.getTimeInMillis());
-
-        Calendar incomeTimeOfThirdClient = new GregorianCalendar(2017, 5, 26, 9,40);
-        Calendar outcomeTimeOfThirdClient = new GregorianCalendar(2017, 5, 26, 13,30);
-        Client thirdClient = new Client(incomeTimeOfThirdClient.getTimeInMillis());
-        thirdClient.setOutcome(outcomeTimeOfThirdClient.getTimeInMillis());
-
-        Calendar incomeTimeOfFourthClient = new GregorianCalendar(2017, 5, 26, 12,0);
-        Calendar outcomeTimeOfFourthClient = new GregorianCalendar(2017, 5, 26, 13,50);
-        Client fourthClient = new Client(incomeTimeOfFourthClient.getTimeInMillis());
-        fourthClient.setOutcome(outcomeTimeOfFourthClient.getTimeInMillis());
-
-        Calendar incomeTimeOfFifthClient = new GregorianCalendar(2017, 5, 26, 13,0);
-        Calendar outcomeTimeOfFifthClient = new GregorianCalendar(2017, 5, 26, 13,30);
-        Client fifthClient = new Client(incomeTimeOfFifthClient.getTimeInMillis());
-        fifthClient.setOutcome(outcomeTimeOfFifthClient.getTimeInMillis());
-
-        Calendar incomeTimeOfSixthClient = new GregorianCalendar(2017, 5, 26, 13,10);
-        Calendar outcomeTimeOfSixthClient = new GregorianCalendar(2017, 5, 26, 16,00);
-        Client sixthClient = new Client(incomeTimeOfSixthClient.getTimeInMillis());
-        sixthClient.setOutcome(outcomeTimeOfSixthClient.getTimeInMillis());
-
-        Calendar incomeTimeOfSeventhClient = new GregorianCalendar(2017, 5, 26, 14,0);
-        Calendar outcomeTimeOfSeventhClient = new GregorianCalendar(2017, 5, 26, 18,50);
-        Client seventhClient = new Client(incomeTimeOfSeventhClient.getTimeInMillis());
-        seventhClient.setOutcome(outcomeTimeOfSeventhClient.getTimeInMillis());
-
-        Calendar openingTime = new GregorianCalendar(2017, 5, 26, 8,0);
-        Calendar closingTime = new GregorianCalendar(2017, 5, 26, 20,00);
-        BankTime bankTime = new BankTime(openingTime, closingTime);
-        bankTime.addClient(firstClient);
-        bankTime.addClient(secondClient);
-        bankTime.addClient(thirdClient);
-        bankTime.addClient(fourthClient);
-        bankTime.addClient(fifthClient);
-        bankTime.addClient(sixthClient);
-        bankTime.addClient(seventhClient);
-
-        bankTime.showMaxPeriods();
     }
 }
