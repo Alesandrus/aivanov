0ab6358d80c9f77bc6227f1c3ebdc6ba6af7765e
Alesandrus <raulon7@mail.ru>
2017-08-01 11:59:09 +0300

    Add wait-notify.

---
+++ chapter_007_Multithreading/src/main/java/ru/job4j/wait_notify_notifyall/MyLock.java
@@ -0,0 +1,81 @@
+package ru.job4j.wait_notify_notifyall;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * MyLock.
+ *
+ * @author Alexander Ivanov
+ * @version 1.0
+ * @since 31.07.2017
+ */
+public class MyLock {
+    /**
+     * Boolean for checking lock.
+     */
+    private boolean isLock = false;
+
+    /**
+     * Lock monitor.
+     */
+    public void lock() {
+        try {
+            synchronized (this) {
+                while (isLock) {
+                    wait();
+                }
+                isLock = true;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Unlock monitor.
+     */
+    public void unlock() {
+        synchronized (this) {
+            if (isLock) {
+                isLock = false;
+                notifyAll();
+            } else {
+                throw new IllegalMonitorStateException();
+            }
+        }
+    }
+
+    /**
+     * Main for showing locking and unlocking.
+     * @param args from cmdLine.
+     */
+    public static void main(String[] args) {
+        MyLock lock = new MyLock();
+
+        new Thread(() -> {
+            try {
+                System.out.println("Втростепенный поток ждет пока главный захватит блокировку");
+                TimeUnit.MILLISECONDS.sleep(20);
+                System.out.println("Второстепенный поток пытается захватить блокировку");
+                lock.lock();
+                System.out.println("Второстепенный поток захватил блокировку");
+                System.out.println("Второстепенный поток делаает работу........");
+                lock.unlock();
+                System.out.println("Второстепенный поток отпускает блокировку и заканчивает свою работу");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        lock.lock();
+        System.out.println("Основной поток захватил блокировку");
+        try {
+            System.out.println("Основной поток засыпает ненадолго");
+            TimeUnit.MILLISECONDS.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        lock.unlock();
+        System.out.println("Основной поток отпускает блокировку и заканчивает свою работу");
+    }
+}
---
+++ chapter_007_Multithreading/src/main/java/ru/job4j/wait_notify_notifyall/mythreadpool/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author Alexander Ivanov
+ * @version 1.0
+ * @since 31.07.2017
+ */
+package ru.job4j.wait_notify_notifyall.mythreadpool;
