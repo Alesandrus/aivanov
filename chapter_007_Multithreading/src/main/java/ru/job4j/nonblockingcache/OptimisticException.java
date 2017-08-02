f68f0a726dd6a8d5cc23e4dc11bbf1fe00d67745
Alesandrus <raulon7@mail.ru>
2017-08-02 14:38:46 +0300

    Add NonBlockingCache.

---
+++ chapter_007_Multithreading/src/main/java/ru/job4j/nonblockingcache/OptimisticException.java
@@ -0,0 +1,11 @@
+package ru.job4j.nonblockingcache;
+
+/**
+ * OptimisticException.
+ *
+ * @author Alexander Ivanov
+ * @version 1.0
+ * @since 01.08.2017
+ */
+public class OptimisticException extends RuntimeException {
+}
