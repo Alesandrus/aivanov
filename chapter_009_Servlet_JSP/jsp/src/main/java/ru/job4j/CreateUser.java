9306cd49f1352aeff78214f9dfd482955e2f3c19
Alesandrus <raulon7@mail.ru>
2017-11-22 21:36:28 +0300

    CRUD_Add encoding POST parameters to UTF-8.

--- chapter_009_Servlet_JSP/jsp/src/main/java/ru/job4j/CreateUser.java
+++ chapter_009_Servlet_JSP/jsp/src/main/java/ru/job4j/CreateUser.java
@@ -3,13 +3,10 @@ package ru.job4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
